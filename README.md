5000c8f1f81d11c868d94463e70f7fe97b5d4b71
Mario Toledo <mario.toledo@idp-software.com.br>
2020-07-29 12:04:22 -0300

    Project initialization based from template

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">
+  Phaser 3 Arkanoid/Breakout TypeScript Example
+</h1>
+
+<h4 align="center">An example of Arkanoid/Breakout game made using Phaser 3 and Typescript.</h4>
---
+++ webpack/credits.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author          Mario Toledo (https://github.com/mariotoledo)
+ * @copyright       2020 Mario Toledo
+ */
+
+console.log(
+  '%c %c %c %c %c Phaser Arkanoid made with Typescript %c https://github.com/mariotoledo/arkanoid-phaser',
+  'background: #ff0000',
+  'background: #ffff00',
+  'background: #00ff00',
+  'background: #00ffff',
+  'color: #fff; background: #000000;',
+  'background: none'
+)
